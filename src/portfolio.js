bacb5fc5c8a67f9fea16c9bf683f745b620a31d3
happi89 <farhaanp99@gmail.com>
2022-07-15 15:09:23 -0400

    commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,18 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://happi89.github.io/cleanfolio',
+  title: 'FP.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
-  description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  name: 'Farhaan Patel',
+  role: 'Fullstack Web Developer',
+  description: '.',
   resume: 'https://example.com',
   social: {
     linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    github: 'https://github.com/happi89',
   },
 }
 
@@ -51,21 +50,28 @@ const skills = [
   // if there are no skills, Skills section won't show up
   'HTML',
   'CSS',
+  'Tailwind',
   'JavaScript',
   'TypeScript',
   'React',
   'Redux',
-  'SASS',
   'Material UI',
   'Git',
   'CI/CD',
   'Jest',
   'Enzyme',
+  'Node',
+  'Express',
+  'MongoDB',
+  'MySQL',
+  'GraphQL',
+  'React Native',
+  'Docker',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'farhaanpatel99@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
